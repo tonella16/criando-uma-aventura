081d9925f999b927b596bb6919d50fbaed5deb5d
tonella16 <antonella.caprioli@escola.pr.gov.br>
2025-11-05 08:13:47 -0300

    Update scripit.js

--- scripit.js
+++ scripit.js
@@ -1,2 +1,11 @@
-  else:
-    print("Não tem ingressos suficientes para todos.")
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
